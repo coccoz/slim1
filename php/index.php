1dddef54d8c4f3a4757caeefce887a4067b24525
cosimo <ballerini.co@gmail.com>
2025-03-31 12:05:35 +0200

    inizio

--- php/index.php
+++ php/index.php
@@ -6,6 +6,19 @@ require __DIR__ . '/controllers/AlunniController.php';
 
 $app = AppFactory::create();
 
+//curl http://localhost:8080/alunni
 $app->get('/alunni', "AlunniController:index");
+
+//curl http://localhost:8080/alunni/2
+$app->get('/alunni/{id:\d+}', "AlunniController:show");
+
+//curl -X POST http://localhost:8080/alunni -H "Content-Type: application/json" -d '{"nome": "ciccio"}
+$app->get('/alunni', "AlunniController:create");
+
+//curl -X POST http://localhost:8080/alunni -H "Content-Type: application/json" -d '{"nome": "ciccio"}
+$app->get('/alunni', "AlunniController:create");
+
+//curl -X POST http://localhost:8080/alunni2 -H "Content-Type: application/json" -d '{"nome": "ciccio"}
+$app->get('/alunni', "AlunniController:update");
 
 $app->run();
